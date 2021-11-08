d5c0a46ea462a8bf996326bd6f5f999ba132a286
0xNB <niklasbetten98@gmail.com>
2021-11-08 13:36:04 +0100

    new activities for backing up data and such

---
+++ app/src/main/java/de/nb/spotter/BackupActivity.kt
@@ -0,0 +1,11 @@
+package de.nb.spotter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BackupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_backup)
+    }
+}
